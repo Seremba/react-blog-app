1d1024429bf46dd53e64f6d9a5a64791e8f14b70
Seremba Patrick <pserembae@gmail.com>
2022-05-16 14:06:04 +0300

    refactored Home and Nav components using context api

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import Feed from './Feed';
+import { useContext } from 'react'
+import DataContext from './context/DataContext'
 
-const Home = ({ posts, fetchError, isLoading }) => {
+const Home = () => {
+    const { searchResult, fetchError, isLoading } = useContext(DataContext);
     return (
         <main className="Home">
             {isLoading && <p className="statusMsg">Loading posts...</p>}
             {!isLoading && fetchError && <p className="statusMsg" style={{ color: "red" }}>{fetchError}</p>}
-            {!isLoading && !fetchError && (posts.length ? <Feed posts={posts} /> : <p className="statusMsg">No posts to display.</p>)}
+            {!isLoading && !fetchError && (searchResult.length ? <Feed posts={searchResult} /> : <p className="statusMsg">No posts to display.</p>)}
         </main>
     )
 }
